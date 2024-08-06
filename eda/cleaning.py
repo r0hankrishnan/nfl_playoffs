d1fd4242902abac060a606364cc70053175f722f
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-08-06 16:53:16 -0400

    remove total stats, team, and year and save modelling data

--- eda/cleaning.py
+++ eda/cleaning.py
@@ -3,11 +3,25 @@
 #Packages
 import pandas as pd
 
+
 #Load data
-df = loadData()
+path = "../../nfl/data/nfl_data.xlsx"
+df = pd.read_excel(path)
+year = df["Year"]
 
 #Look at first columns
 df.head()
 
 #Look at desciptive stats
 df.describe()
+
+#Only want per game stats
+#Drop Team, total stats, and Year since those can't be used as predictors
+drop = list(["Team","off_gp","off_total_yds","off_pass_yds","off_rush_yds","off_pts","def_total_yds"
+             ,"def_pass_yds","def_rush_yds","def_pts","Year"])
+dfModel = df.drop(drop, axis = 1)
+
+dfModel.head()
+
+#Save modelling data set to data folder
+dfModel.to_csv("../../nfl/data/modelling.csv")
